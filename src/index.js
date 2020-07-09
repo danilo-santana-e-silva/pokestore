1c06fc3dc56aa056a3e10a27933c2ab28dbacbbf
Danilo Santana e Silva <daniloss@live.com>
2020-07-08 23:51:35 -0300

    All

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// /src/index.js
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Root from './components/Roots'
+
+import './index.css'
+
+ReactDOM.render(<Root />, document.getElementById('root'))
